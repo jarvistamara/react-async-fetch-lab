ccf743164981421a019f7857a1c7ecdea12791ad
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-04-20 22:51:15 -0400

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+    state = {
+        peopleCurrentlyInSpace: []
+    }
+    render() {
+        return (
+            <div>
+                {this.state.peopleCurrentlyInSpace.map(person => person.name)}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                peopleCurrentlyInSpace: data.people
+            })
+        })
+    }
+}
+export default App
